02bdb6c96d5308033592958da75e7077d597e5b7
caesarii <iwangqinghe@gmail.com>
2018-03-08 21:57:00 +0800

    data type

--- teach-your-self-scheme/basic-type.scm
+++ teach-your-self-scheme/basic-type.scm
@@ -32,3 +32,39 @@
 
 (display (real? 22/7))
 (newline)
+
+(display (complex? 2+3i))
+(newline)
+
+; 类型转换
+(display (char->integer #\d))
+(newline)
+
+(display (integer->char 100))
+(newline)
+
+(display (string->list "hello"))
+(newline)
+
+(display (list->string `(#\h #\e #\l #\l #\o)))
+(newline)
+
+(display (vector->list #(1 2 3 4)))
+(newline)
+
+(display (list->vector `(1 2 3 4)))
+(newline)
+
+(display (number->string 123))
+(newline)
+
+(display (string->number "123"))
+(newline)
+
+(display (string->symbol "name"))
+(newline)
+
+(display (symbol->string `name))
+(newline)
+
+(newline)
