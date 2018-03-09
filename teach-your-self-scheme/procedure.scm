8a2d205133d138716562b20213a96dbb7c3fb773
caesarii <iwangqinghe@gmail.com>
2018-03-09 10:58:14 +0800

    过程

--- teach-your-self-scheme/procedure.scm
+++ teach-your-self-scheme/procedure.scm
@@ -17,3 +17,23 @@
 )
 
 (add3 1 2 3 4)
+(newline)
+
+; apply
+(define x `(2 3 4))
+(display(apply + x))
+(newline)
+
+; begin
+(define (display3 arg1 arg2 arg3)
+    (begin
+        (display arg1)
+        (display " ")
+        (display arg2)
+        (display " ")
+        (display arg3)
+        (display " ")
+        (newline)
+    )
+)
+(display3 5 6 7)
