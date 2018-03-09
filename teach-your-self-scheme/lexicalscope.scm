3954a16a46ba2b39477a56a8000ad8fa3409637a
caesarii <iwangqinghe@gmail.com>
2018-03-09 10:58:05 +0800

    词法作用域

---
+++ teach-your-self-scheme/lexicalscope.scm
@@ -0,0 +1,33 @@
+#! /usr/local/bin/guile -s
+!#
+(define (log value)
+    (newline)
+    (display value)
+    (newline)
+)
+
+(define counter 0)
+(define (bump-counter)
+    (set! counter (+ counter 1))
+    ; 返回值
+    counter
+)
+
+(log (bump-counter))
+(log (bump-counter))
+(log (bump-counter))
+
+; let
+(define x 4)
+(let ((x 1)
+    (y 2)
+    (z 3)) 
+    (log(list x y z))
+)
+
+; fluid-let
+(fluid-let ((counter 99))
+    (+ counter 1)
+    (log counter)
+)
+(log counter)
