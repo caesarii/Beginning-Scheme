77cebba2f28c5e30546a4a5cb7e6528f9c6c6392
caesarii <iwangqinghe@gmail.com>
2018-03-10 12:17:51 +0800

    assignment

--- teach-your-self-scheme/temp.scm
+++ teach-your-self-scheme/temp.scm
@@ -1,8 +1,8 @@
 ; 在 l 从查找 o, 返回其索引
 (define list-postion 
     (lambda (o 1) 
-        (let loop ((i 0) (l l))
-            ; l 为 null 返回 #f
+        (define loop
+            (lambda (i l)
             (if (null? l) 
                 #f
                 ; 如果 l 的第一个元素是目标, 返回 i
@@ -11,9 +11,11 @@
                     i
                     (loop (+ i 1) (cdr l))
                 )
-            )
+            ))
         )
     )
 )
+
+
 
 (display (list-postion 2 `(1 2 3 4)))
