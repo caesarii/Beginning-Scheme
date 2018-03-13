20846940cfabed82f4b8eb317ffaa6be3a6f77a3
caesarii <iwangqinghe@gmail.com>
2018-03-13 10:19:46 +0800

    update list-ref

--- SPL/06/list/list-ref.scm
+++ SPL/06/list/list-ref.scm
@@ -5,10 +5,13 @@
 ))
 
 ; 返回指定索引的元素
+; scheme 中的基本遍历形式 不断对 list cdr
 (define list-ref
     (lambda (ls n)
         (if (= n 0)
+            ; 目标
             (car ls)
+            ; 递归
             (list-ref (cdr ls) (- n 1))
         )
     )
