93a6a864a38a5bba8e954ddee5b9497c47e4b05e
caesarii <iwangqinghe@gmail.com>
2018-03-13 09:12:04 +0800

    create list

--- SPL/06/list.scm
+++ SPL/06/list.scm
@@ -1,4 +1,6 @@
-(define length (lambda (x) 
+
+; length2
+(define length2 (lambda (x) 
     (define improper-list
         (lambda () 
             (display "Exception")
@@ -20,3 +22,82 @@
         )
     )
 ))
+
+; list-ref
+(define list-ref2
+    (lambda (ls n)
+        (if (= n 0)
+            (car ls)
+            (list-ref (cdr ls) (- n 1))
+        )
+    )
+)
+
+; list-tail
+(define list-tail2
+    (lambda (ls n)
+        (if (= n 0)
+            ls
+            (list-tail2 (cdr ls) (- n 1))
+        )
+    )
+)
+
+; append
+(define append2 (lambda args 
+    (let f ([ls '()] [args args])
+        (if (null? args)
+            ls
+            (let g ([ls ls])
+                (if (null? ls)
+                    (f (car args) (cdr args))
+                    (cons (car ls) (g (cdr ls)))
+                )
+            )
+        )
+    )
+))
+
+; reverse
+(define reverse (lambda (ls)
+    (let rev ([ls ls] [new '()])
+        (if (null? ls)
+            new
+            (rev (cdr ls) (cons (car ls) new))
+        )
+    )
+)
+
+; memq
+(define memq2 (lambda (x ls)
+        (cond 
+            [(null? ls) #f]
+            [(eq? (car ls) x) ls]
+            [else (memq x (cdr ls))]
+        )   
+    )
+)
+
+; 返回 ls 的一个 tail list, 该tail list 的 car 对 p 过程返回 #t
+(define memp (lambda (p ls)
+    (cond
+        [(null? ls) #f]
+        [(p (car ls)) ls]
+        [else (memp p (cdr ls))]
+    )
+))
+
+; count-occur
+(define count-occur
+    (lambda (x ls)
+        (cond   [(memq x ls) =>
+            (lambda (ls)
+                (+ (count-occur x (cdr ls)) 1))]
+            [else 0]
+        )
+    )
+)
+
+
+
+
