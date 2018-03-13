9fb92d00241fed7b7f4e7e197affa64037a66f8c
caesarii <iwangqinghe@gmail.com>
2018-03-13 16:06:32 +0800

    update recursion

--- SPL/02/recursive.scm
+++ SPL/02/recursive.scm
@@ -94,3 +94,14 @@
     )
 )
 
+(define tree-copy
+    (lambda (tr)
+        (if (not (pair? tr))
+            tr
+            (cons (tree-copy (car tr))
+                (tree-copy (cdr tr))
+            )
+        )
+    )
+)
+
