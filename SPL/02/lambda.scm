c4b7bf92f6b5468829b9957a82184e0571a508c1
caesarii <iwangqinghe@gmail.com>
2018-03-13 15:00:23 +0800

    update lambda

--- SPL/02/lambda.scm
+++ SPL/02/lambda.scm
@@ -10,7 +10,29 @@
 
 ; 自由变量
 (let ([f 
+        ; 过程定义时绑定的 x
         (let ([x 'sam])
             (lambda (y z) (list x y z))
         )])
-        (f 'i 'am))
+    ; 过程调用时绑定的 x
+    (let ([x 'not-sam])
+        (f 'i 'am)
+    )      
+)
+
+; 参数
+; proper list
+(let ([plist (lambda (x y z) 
+        (display x)
+        (display y)
+        (display z))]
+    )
+    (plist 5 6 7)
+)
+
+; 单个变量
+(let ([single (lambda x (display x))])
+    (single 1 2 3 4) ; (1 2 3 4)
+    (single 1 2 `(3 4)) ; (1 2 (3 4))
+)
+
