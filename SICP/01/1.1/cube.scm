3bbc5b069ffa9c8ad8fba78dc682ae399f1b0f92
caesarii <iwangqinghe@gmail.com>
2018-03-20 10:49:57 +0800

    update

--- SICP/01/1.1/cube.scm
+++ SICP/01/1.1/cube.scm
@@ -1,22 +1,24 @@
 (define (cube x)
 
+	; 边界
 	(define (good-enough guess x)
 	     (< (abs (- (* guess guess guess) x)) 0.001)
 	)
 
+	; 猜测值
 	(define (improve guess x)
 	     (/ (+ (/ x (* guess guess)) (* 2 guess)) 3)
 	)
 
+	; 递归
 	(define (cube-iter guess x)
-		; 
 	     (if (good-enough guess x)
-	               guess
-	          (cube-iter (improve guess x) x)
+		    guess
+	        (cube-iter (improve guess x) x)
 	     )
 	)
 	
-     (cube-iter 1.0 x)
+    (cube-iter 1.0 x)
 )
 
 (display (cube 27))
